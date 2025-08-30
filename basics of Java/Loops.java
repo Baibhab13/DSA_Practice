ed157f9876d844486564b714d5fc6299dd5b7381
Baibhab13 <baibhabsahu31@gmail.com>
2025-08-30 20:00:34 +0530

    learned loop running loop

---
+++ basics of Java/Loops.java
@@ -0,0 +1,39 @@
+// Loops 
+// While loop
+// Do while loop
+
+
+//public class Loops {
+   // public static void main(String[] args) {
+       
+        //for(int counter = 0; counter < 11; counter = counter ++ ) {
+            //counter++ = counter = counter = 1
+           // System.out.println(counter++);
+        //}
+       
+       
+        // for(int i = 0; i < 11; i++) {
+            //System.out.println(i);
+        //}
+        
+        //while 
+        
+       // int i = 0;
+        //while(i < 11) {
+          //  System.out.println(i);
+            //i = i + 1;
+        //}
+
+        //do while
+
+       // int i = 0;
+        //do {
+           // System.out.println(i);
+           // i = i + 1; i++;
+       // }while(i < 11);
+           
+        
+   // }
+//}
+
+
